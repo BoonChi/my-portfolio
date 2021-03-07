c78f524604ab14bcf8374c9ca1c98ed81cb71903
boon chi <hum.boon.chi@gmail.com>
2021-03-07 22:39:12 +0800

    tune landing and interest page

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import './LandingPage.css'
-// import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import Penguin from '../penguin/Penguin'
-// import AboutMe from '../aboutMe/AboutMe';
-// import Interest from '../interest/Interest';
-// import Contact from '../contact/Contact';
+import './LandingPage.css';
 import Description from '../description/Description'
 const LandingPage: React.FunctionComponent = () => {
     return (
             <div className="landing-content">
-                <Penguin></Penguin>
                 <Description></Description>
             </div>
     )
