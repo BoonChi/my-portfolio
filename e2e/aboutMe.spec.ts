98091b586c104bf91aa8ebdef54edcabd3fe3ee9
boon chi <hum.boon.chi@gmail.com>
2022-02-02 10:22:41 +0100

    🐛 create production build before start production server

--- e2e/aboutMe.spec.ts
+++ e2e/aboutMe.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
 
-test('should navigate to the aboutMe page', async ({ page, baseURL }) => {
+test('should navigate to the aboutMe page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('/' + baseURL)
+  await page.goto('/')
   // Find an element with the text 'About Page' and click on it
   await page.click('text=About Me')
   // The new url should be "/about" (baseURL is used there)
