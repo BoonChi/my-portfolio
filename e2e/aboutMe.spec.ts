f483898cd36508a73a7cf61f954f187641422cdc
boon chi <hum.boon.chi@gmail.com>
2022-02-02 10:38:25 +0100

    🐛 set baseUrl in aboutMe test to avoid test failure

--- e2e/aboutMe.spec.ts
+++ e2e/aboutMe.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect } from '@playwright/test'
 
 const isProd = process.env.NODE_ENV === 'production'
+const baseUrl = !isProd ? '' : "/my-portfolio"
 
 test('should navigate to the aboutMe page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto(!isProd ? '' : '/my-portfolio')
+  await page.goto('/')
   // Find an element with the text 'About Page' and click on it
   await page.click('text=About Me')
   // The new url should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL('/aboutMe')
+  await expect(page).toHaveURL(baseUrl + '/aboutMe')
   // The new page should contain an h1 with "About Page"
   await expect(page.locator('h1')).toContainText('About Chi')
 })
