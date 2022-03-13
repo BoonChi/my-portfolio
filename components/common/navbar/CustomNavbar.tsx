e40ebe00dd40032818bfce1f9e94737426c41a8d
chi_passionate_coder <64635043+BoonChi@users.noreply.github.com>
2022-03-13 11:19:41 +0100

    Update CustomNavbar.tsx

--- components/common/navbar/CustomNavbar.tsx
+++ components/common/navbar/CustomNavbar.tsx
@@ -15,7 +15,7 @@ type Props = {
 
 const CustomNavbar: React.FC<Props> = ({ navItems }) => {
   return (
-    <div className="">
+    <div style={{height:"20vh"}}>
       <nav className="navbar navbar-light">
         {navItems.map((nav, index) => (
           <Link href={nav.link} passHref key={index}>
